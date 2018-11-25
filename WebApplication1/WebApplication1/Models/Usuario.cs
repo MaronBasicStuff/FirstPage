1c0cc335d61f7949112dd5abe331d00652b89774
miguelmc0217 <miguelmc0217@users.noreply.github.com>
2018-11-25 03:04:33 -0400

    Primer 1.1

---
+++ WebApplication1/WebApplication1/Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Usuario
+    {
+        public int UsuarioID { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Privilegio { get; set; }
+        public String Email { get; set; }
+        public String Pass { get; set; }
+        public String Phone { get; set; }
+    }
+}
