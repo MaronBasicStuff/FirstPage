46b6f9581b93eeba0ef214a5e93140e8f7d7d781
miguelmc0217 <miguelmc0217@users.noreply.github.com>
2018-11-27 15:12:10 -0400

    Algo hecho

---
+++ WebApplication1/WebApplication1/Controllers/AnunciosController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class AnunciosController : Controller
+    {
+        private FirstPageEntities db = new FirstPageEntities();
+
+        // GET: Anuncios
+        public ActionResult Index()
+        {
+            return View(db.Anuncios.ToList());
+        }
+
+        // GET: Anuncios/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Anuncio anuncio = db.Anuncios.Find(id);
+            if (anuncio == null)
+            {
+                return HttpNotFound();
+            }
+            return View(anuncio);
+        }
+
+        // GET: Anuncios/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Anuncios/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PostID,Foto,Entrada,Precio,Descript,Located,Fecha,UsuarioID")] Anuncio anuncio)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Anuncios.Add(anuncio);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(anuncio);
+        }
+
+        // GET: Anuncios/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Anuncio anuncio = db.Anuncios.Find(id);
+            if (anuncio == null)
+            {
+                return HttpNotFound();
+            }
+            return View(anuncio);
+        }
+
+        // POST: Anuncios/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PostID,Foto,Entrada,Precio,Descript,Located,Fecha,UsuarioID")] Anuncio anuncio)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(anuncio).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(anuncio);
+        }
+
+        // GET: Anuncios/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Anuncio anuncio = db.Anuncios.Find(id);
+            if (anuncio == null)
+            {
+                return HttpNotFound();
+            }
+            return View(anuncio);
+        }
+
+        // POST: Anuncios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Anuncio anuncio = db.Anuncios.Find(id);
+            db.Anuncios.Remove(anuncio);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
