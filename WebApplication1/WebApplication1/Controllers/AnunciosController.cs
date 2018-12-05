b1a6ec6b1cf82632114b1416fabf04604bfa70c7
miguelmc0217 <miguelmc0217@users.noreply.github.com>
2018-12-05 16:22:50 -0400

    sss
    dddd

--- WebApplication1/WebApplication1/Controllers/AnunciosController.cs
+++ WebApplication1/WebApplication1/Controllers/AnunciosController.cs
@@ -31,32 +31,12 @@ namespace WebApplication1.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Usuario usuario = db.Usuarios.Find(Session["usuarioID"]);
             Anuncio anuncio = db.Anuncios.Find(id);
-            
-            
             if (anuncio == null)
             {
                 return HttpNotFound();
             }
             return View(anuncio);
-        }
-
-        public ActionResult AnunciosAdmin()
-        {
-            return View(db.Anuncios.ToList());
-            //return Session["usuarioID"].ToString();
-            // db.Usuarios.FirstOrDefault(u => u.UsuarioID == (int)Session["usuarioID"])
-        }
-
-        public ActionResult Search(string searchElement)
-        {
-            var searchEl = from s in db.Anuncios select s;
-            if (!String.IsNullOrEmpty(searchElement))
-            {
-                searchEl = searchEl.Where(c => c.Entrada.Contains(searchElement));
-            }
-            return View(searchEl);
         }
 
         // GET: Anuncios/Create
@@ -85,7 +65,7 @@ namespace WebApplication1.Controllers
                     Descript = anuncio.Descript,
                     Located = anuncio.Located,
                     Fecha = anuncio.Fecha,
-                    UsuarioID = (int) Session["usuarioID"]
+                    UsuarioID = anuncio.UsuarioID
 
                 });
                 db.SaveChanges();
